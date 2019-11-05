74f2bb202faf37b6badb6ce2ccad9b0f4d1fdbd1
bruce <iswufe99@foxmail.com>
2019-11-05 16:53:30 +0800

    增加了c/1.c

---
+++ c/1.c
@@ -0,0 +1,27 @@
+/*
+2019-11-5
+测试%d %x的输出
+*/
+#include <stdio.h>
+#include <stdlib.h>//system使用需要库文件
+int main(void){
+
+    // system("chcp 65001");//切换终端的字符集为UTF-8
+    system("cls");//清屏,为程序需显示做准备
+ 
+    int i;
+    printf("*********************************\n");
+    printf("*特别程序安装提示,请选择你的参数*\n");
+    printf("*********************************\n");
+    printf("1.安装程序\n");
+    printf("2.修复程序\n");
+    printf("3.卸载程序\n");
+    printf("4.提交bug给作者:iswufe99@foxmail.com\n");
+    printf("请输入你要转换的数:");
+    scanf("m%d",&i);
+    printf("十进制%d\n",i);
+    printf("十六进制%x\n",i);
+    printf("字符%c\n",i);
+    
+    return 0;
+}
